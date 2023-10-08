9ef3f4cb09029acf76ba9d9453d45bb4b51d8c30
hovelopin <kimhojin3714@naver.com>
2023-10-08 11:38:21 +0900

    [KB-111] feat : 임시 비밀번호 전송 완료 UI 완성

--- src/app/find-password/components/password-complete/index.tsx
+++ src/app/find-password/components/password-complete/index.tsx
@@ -1,3 +1,30 @@
+'use client';
+
+import MainButton from '@/components/Button/MainButton';
+import CHeader from '@/components/c-header';
+import { useRouter } from 'next/navigation';
+import * as S from './page.styled';
+
 export default function PasswordComplete() {
-  return <div>패스워드 완료</div>;
+  const { push } = useRouter();
+
+  return (
+    <>
+      <CHeader title="임시 비밀번호 전송" isBackBtn />
+
+      <S.Wrapper>
+        <S.Title>
+          입력하신 이메일 주소로 <br />
+          임시 비밀번호를 전송했어요.
+        </S.Title>
+        <S.SubTitle>
+          입력하신 이메일 주소로 임시 비밀번호를 <br />
+          전송했어요. 확인 후 로그인 해주세요.
+        </S.SubTitle>
+        <S.NextButtonWrapper>
+          <MainButton btnText="로그인" disabled={false} onClick={() => push('/login')} />
+        </S.NextButtonWrapper>
+      </S.Wrapper>
+    </>
+  );
 }
