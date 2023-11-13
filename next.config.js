1acd3b656279f32c0bb2319cc76ef104a8411ca8
yejiiha <1126yezy@gmail.com>
2023-11-13 21:10:32 +0900

    removeConsole 추가

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
   },
   compiler: {
     styledComponents: true,
+    removeConsole: process.env.NODE_ENV === 'production',
   },
   webpack: config => {
     config.module.rules.push({
