2cdc104dacfb96bab2d2db5cdad9b7566747dba5
yejiiha <1126yezy@gmail.com>
2024-07-14 15:23:46 +0900

    feat: MainButton TailwindCSS 적용

--- src/components/Button/MainButton/index.tsx
+++ src/components/Button/MainButton/index.tsx
@@ -1,10 +1,18 @@
 import { ButtonHTMLAttributes } from 'react';
-import * as S from './page.styled';
 
 export interface MainButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   btnText: string;
 }
 
 export default function MainButton({ btnText, ...rest }: MainButtonProps) {
-  return <S.Button {...rest}>{btnText}</S.Button>;
+  return (
+    <button
+      {...rest}
+      className={`flex h-60 w-full items-center justify-center bg-contain bg-center bg-no-repeat text-lg font-normal text-white
+    ${rest.disabled ? 'text-yellow-300 bg-[url("/image/Button/Main_Button_disabled.svg")]' : 'bg-[url("/image/Button/Main_Button_default.svg")] text-white'}
+    ${!rest.disabled && 'active:bg-[url("/image/Button/Main_Button_pressed.svg")]'}`}
+    >
+      {btnText}
+    </button>
+  );
 }
