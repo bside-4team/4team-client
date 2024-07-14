733b5abd7b5581a5eeda21fef89c77089b928f16
hovelopin <kimhojin3714@naver.com>
2024-07-15 00:07:51 +0900

    [TAS-230] fix: 지도 이동 에러 수정 완료

--- src/app/reviews/browse/page.tsx
+++ src/app/reviews/browse/page.tsx
@@ -9,11 +9,6 @@ import { debounce } from 'es-toolkit';
 import { useEffect, useState } from 'react';
 import useMapDataQuery, { MapData } from './hooks/useMapDataQuery';
 
-interface IPosition {
-  title: string;
-  address: string;
-}
-
 type LatLng = {
   lat: number;
   lng: number;
@@ -25,7 +20,9 @@ export default function ReviewBrowse() {
     lng: 126.9254755390151,
   });
 
+  const [loadComplete, setLoadComplete] = useState(false);
   const { openModal } = useModal();
+
   const { mapData } = useMapDataQuery({ ...center });
 
   const modal = (mapData?: MapData[]) => {
@@ -42,99 +39,93 @@ export default function ReviewBrowse() {
       removeExpandBtn: !mapData || !(mapData?.length > 1),
     });
   };
+
   const getKakaoMap = (mapDataList: MapData[]) => {
-    // script가 완전히 load 된 이후, 실행될 함수
-    const onLoadKakaoMap = () => {
-      window.kakao.maps.load(() => {
-        const mapContainer = document.getElementById('map');
-        const mapOption = {
-          center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-          level: 3, // 지도의 확대 레벨
-        };
-
-        const map = new window.kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
-
-        // 지도가 이동할때마다 해당 좌표값을 가져오는 코드!
-        window.kakao.maps.event.addListener(
-          map,
-          'bounds_changed',
-          debounce(() => {
-            const center = map.getCenter();
-
-            setCenter({
-              lat: center.Ma,
-              lng: center.La,
-            });
-          }, 500)
-        );
-
-        const setBounds = () => {
-          // LatLngBounds 객체에 추가된 좌표들을 기준으로 지도의 범위를 재설정합니다
-          // 이때 지도의 중심좌표와 레벨이 변경될 수 있습니다
-          map.setBounds(bounds);
-        };
-
-        // 지도를 재설정할 범위정보를 가지고 있을 LatLngBounds 객체를 생성합니다
-        var bounds = new kakao.maps.LatLngBounds(); // 추가한 코드
-
-        const coordMap: { [key: string]: MapData[] } = {};
-
-        mapDataList?.forEach(mapData => {
-          const coords = new window.kakao.maps.LatLng(mapData.latitude, mapData.longitude);
-          const coordKey = `${mapData.latitude},${mapData.longitude}`;
-          if (!coordMap[coordKey]) {
-            coordMap[coordKey] = [];
-          }
-
-          coordMap[coordKey].push(mapData); // 동일 좌표인 경우 묶기
-
-          var imageSrc = '/image/Map/map.svg', // 마커이미지의 주소입니다
-            imageSize = new kakao.maps.Size(32, 32), // 마커이미지의 크기입니다
-            imageOption = { offset: new kakao.maps.Point(14, 32) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-          var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
-
-          // 결과값으로 받은 위치를 마커로 표시합니다
-          var marker = new window.kakao.maps.Marker({
-            map: map,
-            position: coords,
-            image: markerImage,
-          });
-
-          marker.setMap(map);
-
-          window.kakao.maps.event.addListener(marker, 'click', () => modal(coordMap[coordKey])); // 마커 클릭 이벤트 추가
-
-          // LatLngBounds 객체에 좌표를 추가합니다
-          bounds.extend(coords); //추가한 코드, 현재 코드에서 좌표정보는 point[i]가 아닌 coords이다.
-
-          // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-          setBounds(); //추가한 코드
-        });
-      });
+    const mapContainer = document.getElementById('map');
+    const mapOption = {
+      center: new window.kakao.maps.LatLng(center.lat, center.lng), // 지도의 중심좌표
+      level: 3, // 지도의 확대 레벨
     };
-    // DOM을 이용하여 script 태그를 만들어주자.
-    const mapScript = document.createElement('script');
-    // script.async = true 라면,
-    // 해당 스크립트가 다른 페이지와는 비동기적으로 동작함을 의미한다.
-    mapScript.async = true;
-    // script.src에 map을 불러오는 api를 넣어주자.
-    // 여기에서 우리가 기존에 발급 받았던 apiKey를 넣어주면 된다.
-    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_KEY}&libraries=services&autoload=false`;
 
-    // 이제 우리가 만든 script를 document에 붙여주자.
-    document.head.appendChild(mapScript);
+    const map = new window.kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
-    // sciprt가 완전히 load 된 이후, 지도를 띄우는 코드를 실행시킨다.
-    mapScript.addEventListener('load', () => {
-      window.kakao.maps.load(onLoadKakaoMap);
+    // 지도가 이동할때마다 해당 좌표값을 가져오는 코드!
+    window.kakao.maps.event.addListener(
+      map,
+      'bounds_changed',
+      debounce(() => {
+        const center = map.getCenter();
+        setCenter({
+          lat: center.Ma,
+          lng: center.La,
+        });
+      }, 500)
+    );
+
+    // 지도를 재설정할 범위정보를 가지고 있을 LatLngBounds 객체를 생성합니다
+    var bounds = new kakao.maps.LatLngBounds(); // 추가한 코드
+
+    const coordMap: { [key: string]: MapData[] } = {};
+
+    mapDataList?.forEach(mapData => {
+      const coords = new window.kakao.maps.LatLng(mapData.latitude, mapData.longitude);
+      const coordKey = `${mapData.latitude},${mapData.longitude}`;
+      if (!coordMap[coordKey]) {
+        coordMap[coordKey] = [];
+      }
+
+      coordMap[coordKey].push(mapData); // 동일 좌표인 경우 묶기
+
+      var imageSrc = '/image/Map/map.svg', // 마커이미지의 주소입니다
+        imageSize = new kakao.maps.Size(32, 32), // 마커이미지의 크기입니다
+        imageOption = { offset: new kakao.maps.Point(14, 32) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+      var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+
+      // 결과값으로 받은 위치를 마커로 표시합니다
+      var marker = new window.kakao.maps.Marker({
+        map: map,
+        position: coords,
+        image: markerImage,
+      });
+
+      marker.setMap(map);
+
+      window.kakao.maps.event.addListener(marker, 'click', () => modal(coordMap[coordKey])); // 마커 클릭 이벤트 추가
+
+      // LatLngBounds 객체에 좌표를 추가합니다
+      bounds.extend(coords); //추가한 코드, 현재 코드에서 좌표정보는 point[i]가 아닌 coords이다.
     });
   };
 
   useEffect(() => {
-    if (mapData) {
-      getKakaoMap(mapData.data);
+    if (loadComplete) {
+      if (mapData) {
+        getKakaoMap(mapData.data);
+      }
     }
-  }, [mapData]);
+  }, [loadComplete, mapData]);
+
+  useEffect(() => {
+    const kakaoMapScript = document.createElement('script');
+    kakaoMapScript.async = false;
+    kakaoMapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_KEY}&autoload=false`;
+    document.head.appendChild(kakaoMapScript);
+
+    const onLoadKakaoAPI = () => {
+      window.kakao.maps.load(() => {
+        var container = document.getElementById('map');
+        var options = {
+          center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+          level: 3,
+        };
+
+        var map = new window.kakao.maps.Map(container, options);
+        setLoadComplete(true);
+      });
+    };
+
+    kakaoMapScript.addEventListener('load', onLoadKakaoAPI);
+  }, []);
 
   return (
     <>
