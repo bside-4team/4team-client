6ebba162aa56074f9b9c6757f50cc7969b7a77d1
yejiiha <1126yezy@gmail.com>
2024-07-14 15:24:11 +0900

    feat: error 페이지 TailwindCSS 적용

--- src/components/c-error/index.tsx
+++ src/components/c-error/index.tsx
@@ -3,7 +3,6 @@ import IC_READY from '@/assets/common/img_ready.svg';
 import { useRouter } from 'next/navigation';
 import MainButton from '../Button/MainButton';
 import CHeader from '../c-header';
-import * as S from './page.styled';
 
 interface Props {
   type: '404' | 'ready';
@@ -16,23 +15,25 @@ export default function CError({ type }: Props) {
     <>
       <CHeader title="" isBackBtn />
 
-      <S.Container>
+      <div className="mt-60 flex flex-col items-center justify-center gap-37 px-20 pb-40 pt-0">
         {type === '404' ? <IC_ERROR /> : <IC_READY />}
 
-        <S.TextContainer>
-          <S.Title>{type === '404' ? '페이지 경로가 올바르지 않습니다.' : '서비스 준비중입니다.'}</S.Title>
+        <div className="mb-138 flex flex-col items-center justify-center gap-15">
+          <div className="text text-center text-20 font-bold leading-[150%]">
+            {type === '404' ? '페이지 경로가 올바르지 않습니다.' : '서비스 준비중입니다.'}
+          </div>
 
-          <S.Desc>
+          <p className="whitespace-pre-line break-keep px-20 text-center text-14 leading-[160%] text-neutral-bg40">
             {type === '404'
               ? '접근 방법이 잘못되어 페이지를 연결할 수 없습니다. 이용에 불편을 드려 죄송합니다.'
               : '현재 페이지는 서비스를 준비하고 있어요.\n빠른 시일 내에 만나뵐 수 있도록\n최선을 다 할게요 :)'}
-          </S.Desc>
-        </S.TextContainer>
-      </S.Container>
+          </p>
+        </div>
+      </div>
 
-      <S.NextButtonWrapper>
+      <div className="absolute bottom-0 left-0 w-full bg-white px-20 pb-40 pt-0 mobile:fixed">
         <MainButton btnText="홈 화면으로" onClick={() => router.push('/')} />
-      </S.NextButtonWrapper>
+      </div>
     </>
   );
 }
