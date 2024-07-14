5f51f377161008525e65f130e870a12ef54f858a
yejiiha <1126yezy@gmail.com>
2024-07-14 15:24:02 +0900

    feat: RefreshButton TailwindCSS 적용

--- src/components/Button/RefreshButton/index.tsx
+++ src/components/Button/RefreshButton/index.tsx
@@ -1,8 +1,7 @@
 import IC_DINNER from '@/assets/common/dinner.svg';
 import IC_EXCEPT from '@/assets/common/except.svg';
 import IC_REFRESH from '@/assets/common/refresh_default.svg';
-import { ButtonHTMLAttributes } from 'react';
-import * as S from './pagd.styled';
+import { ButtonHTMLAttributes, useState } from 'react';
 
 interface RefreshButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   btnText: string;
@@ -10,11 +9,18 @@ interface RefreshButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 export default function RefreshButton({ btnText, iconType = 'refresh', ...rest }: RefreshButtonProps) {
+  const [isClicked, setIsClicked] = useState(false);
+
   return (
-    <S.RefreshContainer {...rest}>
+    <button
+      {...rest}
+      onMouseDown={() => setIsClicked(true)}
+      onMouseUp={() => setIsClicked(false)}
+      className={`cursor-${rest.disabled ? 'not-allowed' : 'pointer'} mx-auto mb-18 mt-24 flex w-full max-w-[166px] items-center justify-center gap-2.5 p-3 opacity-${rest.disabled ? '40' : '100'}`}
+    >
       {iconType === 'except' ? <IC_EXCEPT /> : iconType === 'dinner' ? <IC_DINNER /> : <IC_REFRESH />}
 
-      <S.RefreshText>{btnText}</S.RefreshText>
-    </S.RefreshContainer>
+      <span className={`text-14 font-normal ${isClicked ? 'text-secondary-o50' : ''}`}>{btnText}</span>
+    </button>
   );
 }
